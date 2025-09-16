fd7e3d386c2d00374a9196d3c424897c7ee93571
Drasenix <adowns@hotmail.fr>
2025-09-16 16:56:24 +0200

    feat: adding search categories header

--- dougs-categories/src/features/categories/List.js
+++ dougs-categories/src/features/categories/List.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import "../../assets/features/categories/List.css";
 import getVisibleCategories from "../../services/CategorieService";
+import search from "../../assets/features/categories/img/search.png";
 
 function List() {
   useEffect(() => {
@@ -8,18 +9,28 @@ function List() {
   }, []);
 
   return (
-    <>
-      <p>
-        Ceci est un test pour vérifier la
-        couleurssflmsdkmmmmmmmmmslmdkfmslkfmsldfkmsldfksdmlkfsmldfksm d'arrière
-        plan
-      </p>
-      <p>
-        Ceci est un test pour vérifier la
-        couleurssflmsdkmmmmmmmmmslmdkfmslkfmsldfkmsldfksdmlkfsmldfksm d'arrière
-        plan
-      </p>
-    </>
+    <div className="list-categories">
+      <div className="list-categories-header">
+        <div className="list-categories-search">
+          <img
+            className="list-categories-search-btn-img"
+            src={search}
+            alt="Loupe"
+          />
+          <input
+            className="list-categories-search-input"
+            type="text"
+            placeholder="Rechercher une catégorie"
+          />
+        </div>
+        <select className="list-categories-select">
+          <option value="all">Tous les groupes de catégories</option>
+          <option value="one">Groupe 1</option>
+          <option value="two">Groupe 2</option>
+          <option value="three">Groupe 3</option>
+        </select>
+      </div>
+    </div>
   );
 }
 
