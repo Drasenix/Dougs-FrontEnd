f14a396822e213b844f8975abcd524bb94ad3cd8
Drasenix <adowns@hotmail.fr>
2025-09-16 14:50:49 +0200

    feat: adding footer with button select category

--- dougs-categories/src/features/categories/List.js
+++ dougs-categories/src/features/categories/List.js
@@ -6,21 +6,28 @@ import ButtonCategory from "../../components/ui/ButtonCategory";
 
 function List() {
   return (
-    <header className="List-header">
-      <p className="title-categories">Catégories</p>
-      <ButtonCategory
-        class="group-categories-btn"
-        src={group}
-        alt="Boutton qui permet de regrouper les catégories par groupes"
-        text="Groupe de catégorie"
-      ></ButtonCategory>
-      <ButtonCategory
-        class="alphabetical-order-categories-btn"
-        src={alphabetical}
-        alt="Boutton qui permet de trier les catégories par ordre alphabétique"
-        text="Ordre alphabétique"
-      ></ButtonCategory>
-    </header>
+    <>
+      <header className="List-header">
+        <p className="title-categories">Catégories</p>
+        <ButtonCategory
+          class="group-categories-btn"
+          src={group}
+          alt="Boutton qui permet de regrouper les catégories par groupes"
+          text="Groupe de catégorie"
+        ></ButtonCategory>
+        <ButtonCategory
+          class="alphabetical-order-categories-btn"
+          src={alphabetical}
+          alt="Boutton qui permet de trier les catégories par ordre alphabétique"
+          text="Ordre alphabétique"
+        ></ButtonCategory>
+      </header>
+      <footer className="List-footer">
+        <button className="category-select-btn">
+          <p className="category-select-btn-txt">Sélectionner la catégorie</p>
+        </button>
+      </footer>
+    </>
   );
 }
 
