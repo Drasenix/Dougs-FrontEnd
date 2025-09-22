d00e4243e3e2f14b1bfb726132377354eb66b632
Drasenix <adowns@hotmail.fr>
2025-09-22 13:16:18 +0200

    build: add ts-jest

---
+++ dougs-categories/src/tests/services/features/category/CategoryListServiceTest.test.js
@@ -0,0 +1,5 @@
+const CategoryListService = require("../../../../services/features/category/list/CategoryListService.ts");
+
+test("should filter categories and remove those not containing value in wording or description", () => {
+  expect(true).toBe(true);
+});
